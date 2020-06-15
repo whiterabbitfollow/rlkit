f8873a2be200cd6fefcd11a55358727b19368dbc
rstrudel <rstrudel@gmail.com>
2020-06-15 13:41:09 +0200

    rollout_functions: add reset_kwargs, update multiagent

--- rlkit/samplers/rollout_functions.py
+++ rlkit/samplers/rollout_functions.py
@@ -12,6 +12,7 @@ def multitask_rollout(
     representation_goal_key=None,
     get_action_kwargs=None,
     return_dict_obs=False,
+    reset_kwargs=None,
 ):
     if render_kwargs is None:
         render_kwargs = {}
@@ -28,7 +29,10 @@ def multitask_rollout(
     next_observations = []
     path_length = 0
     agent.reset()
-    o = env.reset()
+    if reset_kwargs:
+        o = env.reset(**reset_kwargs)
+    else:
+        o = env.reset()
     if render:
         env.render(**render_kwargs)
     desired_goal = o[desired_goal_key]
@@ -89,8 +93,8 @@ def multiagent_multitask_rollout(
     render=False,
     render_kwargs=None,
     observation_key=None,
-    achieved_goal_key=None,
-    desired_goal_key=None,
+    achieved_q_key=None,
+    desired_q_key=None,
     representation_goal_key=None,
     get_action_kwargs=None,
     reset_kwargs=None,
@@ -119,8 +123,8 @@ def multiagent_multitask_rollout(
         if observation_key:
             s = o[observation_key]
         g = o[representation_goal_key]
-        new_obs = np.hstack((s, g))
-        a, agent_info = agent.get_action(new_obs, **get_action_kwargs)
+        new_o = np.hstack((s, g))
+        a, agent_info = agent.get_action(new_o, **get_action_kwargs)
         next_o, r, d, env_info = env.step(a)
         return a, r, d, next_o, agent_info, env_info
 
@@ -138,16 +142,16 @@ def multiagent_multitask_rollout(
             for k, v in env_info.items():
                 env_infos[idx][k].append(v)
 
-    agents_position = [o[achieved_goal_key], o[desired_goal_key]]
+    agents_q = [o[achieved_q_key], o[desired_q_key]]
     while paths_length < max_path_length:
         # agent0 turn
-        env.set_state_goal(agents_position[0], agents_position[1])
-        o = env.observation(env.get_state())
+        env.set_state_goal(agents_q[0], agents_q[1])
+        o = env.observe()
         if len(observations[0]) > 0:
             next_observations[0].append(o)
         a, r, d, next_o, agent_info, env_info = step_agent(env, agent, o)
         append_to_buffer(0, o, a, r, d, agent_info, env_info)
-        agents_position[0] = next_o[achieved_goal_key]
+        agents_q[0] = next_o[achieved_q_key]
         if render:
             env.render(**render_kwargs)
         paths_length += 1
@@ -156,8 +160,8 @@ def multiagent_multitask_rollout(
             # before getting agent0 observation
             next_observations[0].append(next_o)
             # update agent 1 next_obs after agent 0 move
-            env.set_state_goal(agents_position[1], agents_position[0])
-            o = env.observation(env.get_state())
+            env.set_state_goal(agents_q[1], agents_q[0])
+            o = env.observe()
             if len(observations[1]) > 0:
                 next_observations[1].append(o)
                 rewards[1][-1] = r
@@ -168,21 +172,21 @@ def multiagent_multitask_rollout(
         # agent1 turn
         # switch position and goal of the environment from agent1 perspective
         # and recompute observation
-        env.set_state_goal(agents_position[1], agents_position[0])
-        o = env.observation(env.get_state())
+        env.set_state_goal(agents_q[1], agents_q[0])
+        o = env.observe()
         if len(observations[1]) > 0:
             next_observations[1].append(o)
         a, r, d, next_o, agent_info, env_info = step_agent(env, agent, o)
         append_to_buffer(1, o, a, r, d, agent_info, env_info)
-        agents_position[1] = next_o[achieved_goal_key]
+        agents_q[1] = next_o[achieved_q_key]
         if render:
             env.render(**render_kwargs)
         paths_length += 1
         if d or paths_length == max_path_length:
             # update agent 0 next_obs after agent 1 move
             next_observations[1].append(next_o)
-            env.set_state_goal(agents_position[0], agents_position[1])
-            o = env.observation(env.get_state())
+            env.set_state_goal(agents_q[0], agents_q[1])
+            o = env.observe()
             next_observations[0].append(o)
             rewards[0][-1] = r
             terminals[0][-1] = d
@@ -233,7 +237,12 @@ def plot_paths(paths):
 
 
 def rollout(
-    env, agent, max_path_length=np.inf, render=False, render_kwargs=None,
+    env,
+    agent,
+    max_path_length=np.inf,
+    render=False,
+    render_kwargs=None,
+    reset_kwargs=None,
 ):
     """
     The following value for the following keys will be a 2D array, with the
@@ -256,11 +265,15 @@ def rollout(
     rewards = []
     terminals = []
     agent_infos = []
-    env_infos = []
+    env_infos = {}
     o = env.reset()
     agent.reset()
     next_o = None
     path_length = 0
+    if reset_kwargs:
+        o = env.reset(**reset_kwargs)
+    else:
+        o = env.reset()
     if render:
         env.render(**render_kwargs)
     while path_length < max_path_length:
@@ -271,7 +284,12 @@ def rollout(
         terminals.append(d)
         actions.append(a)
         agent_infos.append(agent_info)
-        env_infos.append(env_info)
+        if not env_infos:
+            for k, v in env_info.items():
+                env_infos[k] = [v]
+        else:
+            for k, v in env_info.items():
+                env_infos[k].append(v)
         path_length += 1
         if d:
             break
@@ -287,6 +305,8 @@ def rollout(
         observations = np.expand_dims(observations, 1)
         next_o = np.array([next_o])
     next_observations = np.vstack((observations[1:, :], np.expand_dims(next_o, 0)))
+    for k, v in env_infos.items():
+        env_infos[k] = np.array(v)
     return dict(
         observations=observations,
         actions=actions,
@@ -294,5 +314,5 @@ def rollout(
         next_observations=next_observations,
         terminals=np.array(terminals).reshape(-1, 1),
         agent_infos=agent_infos,
-        env_infos=np.array(env_infos),
+        env_infos=env_infos,
     )
