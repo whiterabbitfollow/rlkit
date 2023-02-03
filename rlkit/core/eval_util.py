bdad965ed767b3bcae090e061a1342b9adbd36df
berwul <bernhard.f.k.wullt@gmail.com>
2023-02-03 18:08:03 +0100

    apa

--- rlkit/core/eval_util.py
+++ rlkit/core/eval_util.py
@@ -18,7 +18,7 @@ def get_generic_path_information(paths, stat_prefix=""):
     # returns = [sum(path["rewards"]) for path in paths]
     returns = [sum(path["rewards"]) for path in paths]
 
-    rewards = np.vstack([path["rewards"] for path in paths])
+    rewards = np.hstack([path["rewards"] for path in paths])
     statistics.update(
         create_stats_ordered_dict("Rewards", rewards, stat_prefix=stat_prefix)
     )
